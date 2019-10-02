923d3f2e082b16ff0561c9a0cf46eca05c75cfcc
Rodrigo Lima Warmada <dj_rodrigolima@yahoo.com.br>
2019-10-02 19:28:25 -0300

    Adicionado arquivos do projeto Django

---
+++ mysite/polls/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+	return HttpResponse('Olá mundo. você está no index da aplicação polls')
